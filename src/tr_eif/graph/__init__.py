0c3ee9d709141a5cb6c0b664b5ee9f0b42db38ae
Maksym Marnov/Марнов Максим <maximumberlin76@gmail.com>
2026-09-03 14:05:04 +0200

    Add interaction graph structures to __init__.py

--- src/tr_eif/graph/__init__.py
+++ src/tr_eif/graph/__init__.py
@@ -1,1 +1,9 @@
 """Interaction-graph structures and operations for TR-EIF."""
+
+from .interaction import ImageIndex, InteractionEdge, InteractionGraph
+
+__all__ = [
+    "ImageIndex",
+    "InteractionEdge",
+    "InteractionGraph",
+]
