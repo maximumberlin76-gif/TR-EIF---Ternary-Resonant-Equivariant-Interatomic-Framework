9721b1ff8728cb90ea177b5fd2ad504835ad2e72
Maksym Marnov/Марнов Максим <maximumberlin76@gmail.com>
2026-09-03 13:53:05 +0200

    Add atomic configuration data structures to init

--- src/tr_eif/configuration/__init__.py
+++ src/tr_eif/configuration/__init__.py
@@ -1,1 +1,10 @@
 """Atomic configuration data structures for TR-EIF."""
+
+from .state import AtomicConfiguration, Cell3x3, PeriodicAxes, Vector3
+
+__all__ = [
+    "AtomicConfiguration",
+    "Cell3x3",
+    "PeriodicAxes",
+    "Vector3",
+]
