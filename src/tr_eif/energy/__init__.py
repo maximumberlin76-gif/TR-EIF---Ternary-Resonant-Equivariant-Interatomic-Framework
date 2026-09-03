a9ca391ee6299c50da11b3d219f9f0e85320a9a6
Maksym Marnov/Марнов Максим <maximumberlin76@gmail.com>
2026-09-03 16:46:13 +0200

    Add CoordinateDifferentiation to __all__ exports

--- src/tr_eif/energy/__init__.py
+++ src/tr_eif/energy/__init__.py
@@ -1,5 +1,6 @@
 """Conservative energy representations and operations for TR-EIF."""
 
+from .differentiation import CoordinateDifferentiation
 from .force import AtomicForces, ForceState
 from .functional import LinearInvariantEnergyFunctional
 from .model import EnergyModelResult, ReferenceEnergyModel
@@ -8,6 +9,7 @@ from .state import AtomicEnergies, EnergyState
 __all__ = [
     "AtomicEnergies",
     "AtomicForces",
+    "CoordinateDifferentiation",
     "EnergyModelResult",
     "EnergyState",
     "ForceState",
