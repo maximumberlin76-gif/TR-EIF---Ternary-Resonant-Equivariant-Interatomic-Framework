09ea8ff686b3aace77de9ece82bbfb8e4401f65b
Maksym Marnov/Марнов Максим <maximumberlin76@gmail.com>
2026-09-03 13:58:21 +0200

    Add geometric operations to TR-EIF module

--- src/tr_eif/geometry/__init__.py
+++ src/tr_eif/geometry/__init__.py
@@ -1,1 +1,10 @@
 """Geometric operations and representations for TR-EIF."""
+
+from .relative import displacement, distance, squared_distance, unit_direction
+
+__all__ = [
+    "displacement",
+    "distance",
+    "squared_distance",
+    "unit_direction",
+]
