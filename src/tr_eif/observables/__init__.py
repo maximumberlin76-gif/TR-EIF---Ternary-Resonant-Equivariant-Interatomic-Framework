bfcc39976918a78a808995e090f5ca048aaf90cc
Maksym Marnov/Марнов Максим <maximumberlin76@gmail.com>
2026-09-03 17:37:08 +0200

    Add ReplayComparison and compare_trace_replay to exports

--- src/tr_eif/observables/__init__.py
+++ src/tr_eif/observables/__init__.py
@@ -5,6 +5,7 @@ from .json_export import (
     trace_sequence_to_json,
     trace_sequence_to_json_bytes,
 )
+from .replay import ReplayComparison, compare_trace_replay
 from .sequence import TraceSequence
 from .serialization import (
     trace_record_to_mapping,
@@ -14,8 +15,10 @@ from .trace import TraceRecord
 
 __all__ = [
     "PathInput",
+    "ReplayComparison",
     "TraceRecord",
     "TraceSequence",
+    "compare_trace_replay",
     "trace_record_to_mapping",
     "trace_sequence_to_json",
     "trace_sequence_to_json_bytes",
