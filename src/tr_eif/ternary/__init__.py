e32b8811d87e6d61a8b6ecbc319eb71c50c5884c
Maksym Marnov/Марнов Максим <maximumberlin76@gmail.com>
2026-09-03 14:47:44 +0200

    Add TernaryTransition and related functions to __init__

--- src/tr_eif/ternary/__init__.py
+++ src/tr_eif/ternary/__init__.py
@@ -6,10 +6,18 @@ from .state import (
     validate_ternary_state,
     validate_ternary_vector,
 )
+from .transition import (
+    TernaryTransition,
+    is_committed_transition_allowed,
+    is_direct_opposite_transition,
+)
 
 __all__ = [
     "TernaryState",
+    "TernaryTransition",
     "TernaryVector",
+    "is_committed_transition_allowed",
+    "is_direct_opposite_transition",
     "validate_ternary_state",
     "validate_ternary_vector",
 ]
