701357be0241080b819275d64bbf88d1e42263fc
Maksym Marnov/Марнов Максим <maximumberlin76@gmail.com>
2026-09-03 16:01:50 +0200

    Add edge-related functions to __init__.py

--- src/tr_eif/equivariant/__init__.py
+++ src/tr_eif/equivariant/__init__.py
@@ -1,5 +1,10 @@
 """E(3)-equivariant feature representations and operations for TR-EIF."""
 
+from .edge import (
+    EquivariantEdgeInput,
+    edge_input_from_geometry,
+    evaluate_equivariant_edge_input,
+)
 from .features import (
     NodeFeatures,
     NodeFeatureVector,
@@ -10,9 +15,12 @@ from .transform import E3Transformation, Matrix3x3
 
 __all__ = [
     "E3Transformation",
+    "EquivariantEdgeInput",
     "Matrix3x3",
     "NodeFeatures",
     "NodeFeatureVector",
     "ScalarFeatures",
     "VectorFeatures",
+    "edge_input_from_geometry",
+    "evaluate_equivariant_edge_input",
 ]
