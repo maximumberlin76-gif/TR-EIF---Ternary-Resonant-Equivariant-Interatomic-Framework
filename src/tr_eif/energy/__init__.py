77d5d3dfb2abb670cfffc44674f37e4337d7e498
Maksym Marnov/Марнов Максим <maximumberlin76@gmail.com>
2026-09-03 16:42:32 +0200

    Export TR-EIF reference energy model

--- src/tr_eif/energy/__init__.py
+++ src/tr_eif/energy/__init__.py
@@ -1,10 +1,13 @@
 """Conservative energy representations and operations for TR-EIF."""
 
 from .functional import LinearInvariantEnergyFunctional
+from .model import EnergyModelResult, ReferenceEnergyModel
 from .state import AtomicEnergies, EnergyState
 
 __all__ = [
     "AtomicEnergies",
+    "EnergyModelResult",
     "EnergyState",
     "LinearInvariantEnergyFunctional",
+    "ReferenceEnergyModel",
 ]
