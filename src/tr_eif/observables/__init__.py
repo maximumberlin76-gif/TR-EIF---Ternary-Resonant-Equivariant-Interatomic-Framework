62fa14520e9c320cecf4e3243d199973a184e0ff
Maksym Marnov/Марнов Максим <maximumberlin76@gmail.com>
2026-09-03 17:29:59 +0200

    Add TraceSequence and serialization functions to __init__

--- src/tr_eif/observables/__init__.py
+++ src/tr_eif/observables/__init__.py
@@ -1,7 +1,15 @@
 """Observable and trace representations for TR-EIF."""
 
+from .sequence import TraceSequence
+from .serialization import (
+    trace_record_to_mapping,
+    trace_sequence_to_mapping,
+)
 from .trace import TraceRecord
 
 __all__ = [
     "TraceRecord",
+    "TraceSequence",
+    "trace_record_to_mapping",
+    "trace_sequence_to_mapping",
 ]
