f563eb0a35e6f8e4a71ebecfeebb0d929b23e186
Maksym Marnov/Марнов Максим <maximumberlin76@gmail.com>
2026-09-03 14:15:16 +0200

    Add evaluate_edge_geometry to module exports

--- src/tr_eif/graph/__init__.py
+++ src/tr_eif/graph/__init__.py
@@ -1,11 +1,14 @@
 """Interaction-graph structures and operations for TR-EIF."""
 
 from .builder import build_cutoff_graph
+from .geometry import EdgeGeometry, evaluate_edge_geometry
 from .interaction import ImageIndex, InteractionEdge, InteractionGraph
 
 __all__ = [
+    "EdgeGeometry",
     "ImageIndex",
     "InteractionEdge",
     "InteractionGraph",
     "build_cutoff_graph",
+    "evaluate_edge_geometry",
 ]
