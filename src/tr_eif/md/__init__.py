a54b41c96b15b75de2bd14bdc4eaa88bb2fa378f
Maksym Marnov/Марнов Максим <maximumberlin76@gmail.com>
2026-09-03 18:50:54 +0200

    Add __init__.py for molecular dynamics module

---
+++ src/tr_eif/md/__init__.py
@@ -0,0 +1,13 @@
+"""Molecular-dynamics interfaces for TR-EIF."""
+
+from tr_eif.md.state import (
+    AtomicMasses,
+    AtomicVelocities,
+    MolecularDynamicsState,
+)
+
+__all__ = [
+    "AtomicMasses",
+    "AtomicVelocities",
+    "MolecularDynamicsState",
+]
