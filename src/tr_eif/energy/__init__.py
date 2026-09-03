52f3b109bf4e9fe952e20cf087fc68c6a4477b30
Maksym Marnov/Марнов Максим <maximumberlin76@gmail.com>
2026-09-03 16:44:24 +0200

    Add AtomicForces and ForceState to __all__

--- src/tr_eif/energy/__init__.py
+++ src/tr_eif/energy/__init__.py
@@ -1,13 +1,16 @@
 """Conservative energy representations and operations for TR-EIF."""
 
+from .force import AtomicForces, ForceState
 from .functional import LinearInvariantEnergyFunctional
 from .model import EnergyModelResult, ReferenceEnergyModel
 from .state import AtomicEnergies, EnergyState
 
 __all__ = [
     "AtomicEnergies",
+    "AtomicForces",
     "EnergyModelResult",
     "EnergyState",
+    "ForceState",
     "LinearInvariantEnergyFunctional",
     "ReferenceEnergyModel",
 ]
