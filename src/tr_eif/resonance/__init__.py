9c1cbceac26eaf14f81f955cd7744b40c1b6e7eb
Maksym Marnov/Марнов Максим <maximumberlin76@gmail.com>
2026-09-03 14:41:10 +0200

    Add ResonanceDescriptor and related functions

--- src/tr_eif/resonance/__init__.py
+++ src/tr_eif/resonance/__init__.py
@@ -1,6 +1,12 @@
 """Resonance-state representations and dynamics for TR-EIF."""
 
 from .classification import ResonanceRegion, classify_resonance_region
+from .descriptor import (
+    ResonanceDescriptor,
+    frequency_spread,
+    resonance_coordinate,
+    resonance_descriptor,
+)
 from .dynamics import PhaseDerivativeVector, phase_derivatives
 from .integrator import euler_step
 from .order import phase_order_parameter, state_phase_order
@@ -30,14 +36,18 @@ __all__ = [
     "PhaseLagVector",
     "PhaseVector",
     "ResonanceCoordinate",
+    "ResonanceDescriptor",
     "ResonanceRegion",
     "ResonanceSpace",
     "ResonanceState",
     "ResonanceWindow",
     "classify_resonance_region",
     "euler_step",
+    "frequency_spread",
     "phase_derivatives",
     "phase_order_parameter",
+    "resonance_coordinate",
+    "resonance_descriptor",
     "state_phase_order",
     "wrap_phase",
     "wrap_phases",
