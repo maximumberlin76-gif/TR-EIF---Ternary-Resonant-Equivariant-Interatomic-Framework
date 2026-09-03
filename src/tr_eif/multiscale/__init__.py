304d88ca6531f573f6434452626f7ffa08afaf83
Maksym Marnov/Марнов Максим <maximumberlin76@gmail.com>
2026-09-03 21:14:12 +0200

    Implement multiscale interfaces in __init__.py
    This file defines multiscale interfaces for the TR-EIF module, including various imports and the __all__ list for public API exposure.

---
+++ src/tr_eif/multiscale/__init__.py
@@ -0,0 +1,60 @@
+"""Multiscale interfaces for TR-EIF."""
+
+from .composition import compose_partitions
+from .geometry import (
+    CoarsePositions,
+    FineMasses,
+    FinePositions,
+    mass_weighted_centroids,
+)
+from .hierarchy import MultiscaleHierarchy
+from .hierarchy_state import (
+    MultiscaleStateHierarchy,
+    build_multiscale_state_hierarchy,
+)
+from .partition import MultiscalePartition
+from .prolongation import (
+    CoarseScalars,
+    CoarseVectors,
+    FineScalars,
+    FineVectors,
+    prolong_scalar_broadcast,
+    prolong_vector_broadcast,
+)
+from .reduction import (
+    CoarseMasses,
+    reduce_masses,
+    reduce_scalar_sum,
+)
+from .state import (
+    CoarseScaleState,
+    build_coarse_scale_state,
+)
+from .vector_average import mass_weighted_vector_average
+from .vector_reduction import reduce_vector_sum
+
+
+__all__ = [
+    "CoarseMasses",
+    "CoarsePositions",
+    "CoarseScaleState",
+    "CoarseScalars",
+    "CoarseVectors",
+    "FineMasses",
+    "FinePositions",
+    "FineScalars",
+    "FineVectors",
+    "MultiscaleHierarchy",
+    "MultiscalePartition",
+    "MultiscaleStateHierarchy",
+    "build_coarse_scale_state",
+    "build_multiscale_state_hierarchy",
+    "compose_partitions",
+    "mass_weighted_centroids",
+    "mass_weighted_vector_average",
+    "prolong_scalar_broadcast",
+    "prolong_vector_broadcast",
+    "reduce_masses",
+    "reduce_scalar_sum",
+    "reduce_vector_sum",
+]
