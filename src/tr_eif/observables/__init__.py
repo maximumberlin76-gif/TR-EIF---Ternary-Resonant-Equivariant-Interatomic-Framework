ad40fcb50d32861f01182adcb014d81bb53b5fb2
Maksym Marnov/Марнов Максим <maximumberlin76@gmail.com>
2026-09-03 17:32:26 +0200

    Add JSON export functions to __init__.py

--- src/tr_eif/observables/__init__.py
+++ src/tr_eif/observables/__init__.py
@@ -1,5 +1,9 @@
 """Observable and trace representations for TR-EIF."""
 
+from .json_export import (
+    trace_sequence_to_json,
+    trace_sequence_to_json_bytes,
+)
 from .sequence import TraceSequence
 from .serialization import (
     trace_record_to_mapping,
@@ -11,5 +15,7 @@ __all__ = [
     "TraceRecord",
     "TraceSequence",
     "trace_record_to_mapping",
+    "trace_sequence_to_json",
+    "trace_sequence_to_json_bytes",
     "trace_sequence_to_mapping",
 ]
