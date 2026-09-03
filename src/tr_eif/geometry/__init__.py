2617e1e69616f4a462b92bfc2f24da52805bb77c
Maksym Marnov/Марнов Максим <maximumberlin76@gmail.com>
2026-09-03 14:01:27 +0200

    Import periodic functions into geometry module

--- src/tr_eif/geometry/__init__.py
+++ src/tr_eif/geometry/__init__.py
@@ -1,10 +1,21 @@
 """Geometric operations and representations for TR-EIF."""
 
+from .periodic import (
+    cartesian_to_fractional,
+    fractional_to_cartesian,
+    minimum_image_displacement,
+    periodic_image_displacement,
+)
 from .relative import displacement, distance, squared_distance, unit_direction
 
 __all__ = [
+    "cartesian_to_fractional",
     "displacement",
     "distance",
+    "fractional_to_cartesian",
+    "minimum_image_displacement",
+    "periodic_image_displacement",
     "squared_distance",
     "unit_direction",
 ]
+
