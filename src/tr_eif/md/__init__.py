2667dc603a0f6cb472497591e1b9293ff2d3e1ac
Maksym Marnov/Марнов Максим <maximumberlin76@gmail.com>
2026-09-03 19:40:27 +0200

    Expand __init__.py with additional imports and exports

--- src/tr_eif/md/__init__.py
+++ src/tr_eif/md/__init__.py
@@ -1,13 +1,67 @@
 """Molecular-dynamics interfaces for TR-EIF."""
 
+from tr_eif.md.dynamics import (
+    AtomicAccelerations,
+    acceleration_from_force,
+    accelerations_from_forces,
+)
+from tr_eif.md.execution import (
+    MolecularDynamicsStepResult,
+    velocity_verlet_step,
+)
+from tr_eif.md.integrator import (
+    velocity_verlet_position,
+    velocity_verlet_positions,
+    velocity_verlet_velocity,
+    velocity_verlet_velocities,
+)
+from tr_eif.md.kinematics import (
+    advance_position,
+    advance_velocity,
+)
+from tr_eif.md.observables import (
+    AtomicKineticEnergies,
+    KineticEnergyState,
+    MolecularDynamicsEnergyState,
+    kinetic_energy,
+    molecular_dynamics_energy,
+)
 from tr_eif.md.state import (
     AtomicMasses,
     AtomicVelocities,
     MolecularDynamicsState,
 )
+from tr_eif.md.trajectory import (
+    MolecularDynamicsTrajectory,
+    run_velocity_verlet_trajectory,
+)
+from tr_eif.md.trajectory_observables import (
+    MolecularDynamicsTrajectoryEnergy,
+    evaluate_trajectory_energy,
+)
 
 __all__ = [
+    "AtomicAccelerations",
+    "AtomicKineticEnergies",
     "AtomicMasses",
     "AtomicVelocities",
+    "KineticEnergyState",
+    "MolecularDynamicsEnergyState",
     "MolecularDynamicsState",
+    "MolecularDynamicsStepResult",
+    "MolecularDynamicsTrajectory",
+    "MolecularDynamicsTrajectoryEnergy",
+    "acceleration_from_force",
+    "accelerations_from_forces",
+    "advance_position",
+    "advance_velocity",
+    "evaluate_trajectory_energy",
+    "kinetic_energy",
+    "molecular_dynamics_energy",
+    "run_velocity_verlet_trajectory",
+    "velocity_verlet_position",
+    "velocity_verlet_positions",
+    "velocity_verlet_step",
+    "velocity_verlet_velocity",
+    "velocity_verlet_velocities",
 ]
