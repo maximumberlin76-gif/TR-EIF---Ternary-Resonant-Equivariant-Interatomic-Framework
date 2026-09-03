0c1d3bb1fd2461ec5c5b7e32935fdcc2bb549aaa
Maksym Marnov/Марнов Максим <maximumberlin76@gmail.com>
2026-09-03 14:07:43 +0200

    Add build_cutoff_graph to module exports

--- src/tr_eif/graph/__init__.py
+++ src/tr_eif/graph/__init__.py
@@ -1,9 +1,11 @@
 """Interaction-graph structures and operations for TR-EIF."""
 
+from .builder import build_cutoff_graph
 from .interaction import ImageIndex, InteractionEdge, InteractionGraph
 
 __all__ = [
     "ImageIndex",
     "InteractionEdge",
     "InteractionGraph",
+    "build_cutoff_graph",
 ]
